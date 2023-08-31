d17ea2e09b4d2f0570bfc4105b6f9c414eb60ff7
githubuiux <uiuxista@gmail.com>
2023-08-31 11:45:04 +0300

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Levent on 31.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
